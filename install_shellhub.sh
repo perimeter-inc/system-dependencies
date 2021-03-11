b3237ece23c2f2f8a2e35d60dbfd591bb1ab6ee3
af <soda@daslk.com>
2021-03-11 19:27:30 -0300

    .

--- install_shellhub.sh
+++ install_shellhub.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
 if [ "$EUID" -ne 0 ]
@@ -9,14 +9,13 @@ fi
 export SHELLHUB_PATH=/usr/bin/shellhub_agent
 
 function get_shellhub_based_on_user_input() {
-   while true; do
-      read -p "Do you want to build shellhub from sources (y/n)?. If \"n\" is answered, a pre-built version will be downloaded (might not be the lattest version)." yn
-      case $yn in
-         [Yy]* ) build_shellhub_from_sources;;
-         [Nn]* ) download_prebuilt_shellhub;;
-         * ) echo "Please answer y/n (yes/no).";;
-      esac
-   done
+   echo -e "Do you want to build shellhub from sources (y/n)?.\nIf \"n\" is answered, a pre-built version will be downloaded (might not be the lattest version)."
+   read -n1 -p "[y,n]" yn
+   case $yn in  
+      y|Y) build_shellhub_from_sources ;; 
+      n|N) download_prebuilt_shellhub ;; 
+      *) echo "Please answer y/n (yes/no)." ;; 
+   esac
 }
 
 function download_prebuilt_shellhub() {
@@ -29,19 +28,22 @@ function build_shellhub_from_sources() {
    install_go_compiler
    git clone -b v0.5.1 https://github.com/shellhub-io/shellhub.git shellhub
    cd shellhub/agent
+   echo "Building shellhub..."
    go build -ldflags "-X main.AgentVersion=v0.5.1"
 
    cp -f ./agent ${SHELLHUB_PATH}
 }
 
 function install_go_compiler() {
+   echo "Installing Go compiler..."
    wget https://golang.org/dl/go1.16.1.linux-armv6l.tar.gz
    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.1.linux-armv6l.tar.gz
    export PATH=$PATH:/usr/local/go/bin
 }
 
 function install_shellhub_service() {
-      cat << EOF > /etc/systemd/system/shellhub_agent.service
+   echo "Installing shellhub service."
+   cat << EOF > /etc/systemd/system/shellhub_agent.service
 [Unit]
 Description=Shellhub Agent
 
@@ -62,6 +64,7 @@ EOF
 }
 
 function setup_shellhub_service() {
+   echo "Setting up shellhub service."
    sudo systemctl daemon-reload
    sudo systemctl enable shellhub_agent
    sudo systemctl restart shellhub_agent

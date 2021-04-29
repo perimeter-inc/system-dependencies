b7cc9035c5badbb374a050590f4ec10fd60e258c
Leoni Mota Loris <leoniloris1@hotmail.com>
2021-04-29 01:07:08 -0300

    .

--- install_shellhub.sh
+++ install_shellhub.sh
@@ -10,67 +10,13 @@ export SHELLHUB_EXECUTABLE_PATH=/usr/bin/shellhub_agent
 export SHELLHUB_KEYS_FOLDER=/root/keys
 export PREFERRED_HOSTNAME="$(hostname)"
 
-function show_help {
-    echo "Install shellhub."
-    echo "    USAGE: $0 SERVER_ADDRESS TENANT_ID"
-}
-
-function parse_command_line {
-   #  if [ $# -lt 2 ]; then
-   #      show_help
-   #      exit 1
-   #  fi
-    export SHELLHUB_SERVER_ADDRESS="http://ec2-54-183-118-145.us-west-1.compute.amazonaws.com/"
-    export SHELLHUB_TENANT_ID="01dda772-8bdf-49ae-8a86-d75307489613"
-}
-
-function get_shellhub_based_on_user_input() {
-   echo -e "Do you want to build shellhub from sources (y/n)?.\nIf \"n\" is answered, a pre-built version will be downloaded (might not be the lattest version)."
-   read -n1 -p "[y,n]" yn
-   case $yn in
-      y|Y) build_shellhub_from_sources ;;
-      n|N) download_prebuilt_shellhub ;;
-      *) echo "Please answer y/n (yes/no)." || exit 1;;
-   esac
-}
-
-function download_prebuilt_shellhub() {
-   echo "Not implemented!!!"
-   echo "TODO: Infer and download based on the architecture."
-   exit 1
-}
-
-function build_shellhub_from_sources() {
-   install_go_compiler
-   cd ~
-   rm -rf shellhub && git clone -b v0.6.0 https://github.com/shellhub-io/shellhub.git shellhub
-   cd shellhub/agent
-   echo "Building shellhub..."
-   go build -ldflags "-X main.AgentVersion=v0.6.0"
-
-   cp -f ./agent "${SHELLHUB_EXECUTABLE_PATH}"
-}
-
-function install_go_compiler() {
-   echo "Installing Go compiler..."
-   # ARCH="$(uname -m)"
-   # if [ "$ARCH" == "aarch64" ]; then
-   #    export GO_COMPILER_FILE=go1.14.15.linux-arm64.tar.gz
-   # elif [ "$ARCH" == "armv7l" ]; then
-   #    export GO_COMPILER_FILE=go1.14.15.linux-armv6l.tar.gz
-   # else
-   #    echo "Architecture not supported: $(uname -m)"
-   #    exit -1
-   # fi
-
-   # rm -rf "${GO_COMPILER_FILE}" && wget "https://golang.org/dl/${GO_COMPILER_FILE}"
-   rm -rf ~/go/
-   tar -C ~ -xzf ./go1.15.11.linux-armv6l.tar.gz
-   export PATH=$PATH:~/go/bin
-}
+export SHELLHUB_SERVER_ADDRESS="http://ec2-54-183-118-145.us-west-1.compute.amazonaws.com/"
+export SHELLHUB_TENANT_ID="01dda772-8bdf-49ae-8a86-d75307489613"
 
 function install_shellhub_service() {
-   cd ~/shellhub
+   cp -f ./shellhub_agent "${SHELLHUB_EXECUTABLE_PATH}"
+   rm -rf shellhub && git clone -b v0.6.0 https://github.com/shellhub-io/shellhub.git shellhub
+   cd shellhub
    make keygen
    # By now, three files should have been created ssh_private_key, api_public_key and api_private_key.
    mkdir -p "${SHELLHUB_KEYS_FOLDER}"
@@ -108,7 +54,5 @@ function start_shellhub_service() {
    chmod +x "${SHELLHUB_EXECUTABLE_PATH}"
 }
 
-parse_command_line $@
-get_shellhub_based_on_user_input
 install_shellhub_service
 start_shellhub_service
